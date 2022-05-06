d55e77f75e13c757b938462fe2c35500db68ee94
kibar <mustafa@kibar.pro>
2022-05-06 21:18:20 +0300

    initial commit

---
+++ controllers/sudoku-solver.js
@@ -0,0 +1,13 @@
+class SudokuSolver {
+  validate(puzzleString) {}
+
+  checkRowPlacement(puzzleString, row, column, value) {}
+
+  checkColPlacement(puzzleString, row, column, value) {}
+
+  checkRegionPlacement(puzzleString, row, column, value) {}
+
+  solve(puzzleString) {}
+}
+
+module.exports = SudokuSolver;
